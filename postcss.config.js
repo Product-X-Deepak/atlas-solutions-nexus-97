a89c4e300429b81c22191ff16195219e5d888a4d
DEEPAK BASANTIA <dbasantia1665@gmail.com>
2025-05-24 20:13:54 +0530

    Refactore

--- postcss.config.js
+++ postcss.config.js
@@ -1,30 +1,6 @@
-module.exports = {
+export default {
   plugins: {
-    'tailwindcss': {},
-    'autoprefixer': {},
-    'postcss-preset-env': {
-      stage: 3,
-      features: {
-        'nesting-rules': false,
-        'custom-properties': true,
-      },
-      browsers: ['defaults', 'not IE 11']
-    },
-    ...(process.env.NODE_ENV === 'production' 
-      ? {
-          'cssnano': {
-            preset: ['default', {
-              discardComments: {
-                removeAll: true,
-              },
-              discardDuplicates: true,
-              discardEmpty: true,
-              reduceIdents: false,
-              minifyFontValues: { removeQuotes: false },
-              normalizeWhitespace: false
-            }]
-          }
-        } 
-      : {})
-  }
-} 
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
